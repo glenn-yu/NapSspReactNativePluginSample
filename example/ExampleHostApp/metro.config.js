a04c382059ba815a99533023bc5d0bea4312b505
gwangy.claw <gwangy.claw@gwangyclawui-MacBookAir.local>
2026-04-18 08:05:33 +0900

    chore(example): fix metro resolver for workspace imports

--- example/ExampleHostApp/metro.config.js
+++ example/ExampleHostApp/metro.config.js
@@ -6,6 +6,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const path = require('path');
+
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [path.resolve(__dirname, '..', '..')],
+  resolver: {
+    // Allow importing from the package root `src` directory
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(__dirname, '..', '..', 'node_modules', name),
+    }),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
